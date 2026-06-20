5dffa4b4bfc1199ffb3a42d3f681029e194a1787
Tanish2028 <tanishshrimali2023@gmail.com>
2026-06-20 17:31:43 +0530

    Apis

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser'; //to access cookies of the user servers
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
+
+app.use(express.json({limit:"16kb"}))
+
+app.use(express.urlencoded({extended:true,limit:"16kb"})) //url ko encod karne ke liye
+
+app.use(express.static("public")) //storing some files,datas in personal server
+
+app.use(cookieParser());
+export {app}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>{next(err)})
+    }
+}
+
+export {asyncHandler}
+
+//higher order function
+// const asyncHandler = (fn) => async (req,res,next) =>{
+//     try {
+        
+//     } catch (error) {
+//         console.log("Error in Handling : ",error);
+//         res.status(error.code || 500).json({success:false,
+//             message:error.message
+//         })
+//     }
+// }
